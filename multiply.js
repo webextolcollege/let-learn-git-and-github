56895a4b0513296025a09f05d0f426594d4bae99
Aijaz Ali <webextolcollege@gmail.com>
2023-04-03 04:03:35 +0500

    add multiply.js

---
+++ multiply.js
@@ -0,0 +1,8 @@
+const f_Num=7;
+//declare and initaiize variable for first num
+const l_Num=5;
+//declare and initaiize variable for second num
+//calculate multiplication of given two numbers
+const mul=f_Num*l_Num;
+//display the multiplication of two numbers
+console.log('The multiplication of '+f_Num+' and '+l_Num+' is: '+mul);
